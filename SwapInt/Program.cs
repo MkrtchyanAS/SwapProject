78d78a38fa76f2c05371ff1fb8ceda7ecca42618
User <User@K3-512-05>
2017-12-06 20:05:18 +0500

    Тест

--- SwapInt/Program.cs
+++ SwapInt/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SwapInt
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            int a = 5;
+            int b = 1;
+
+            SwapInt(ref a, ref b);
+
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+
+        }
+        /// <summary>
+        /// Take two variables and switch their values.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private static void SwapInt(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
         }
     }
 }
